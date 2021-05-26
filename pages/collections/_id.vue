197d3f5e4abe208cf7d5c1c5a0a2222b8c2f12bf
Sven <sfolkerts89@gmail.com>
2021-05-26 22:28:38 +0200

    Fiddling with _id page

--- pages/collections/_id.vue
+++ pages/collections/_id.vue
@@ -1,19 +1,24 @@
 <template>
-  <div v-if="$apollo.loading">
-    <div class="apollo-loading">
-      <h2>Im still loading..</h2>
-    </div>
-  </div>
-  <div v-else>
+  <!-- <div v-if="!$apollo.loading">
     <div v-bind="collection" class="collection-container">
       <h1>{{ collection.title }}</h1>
       <div class="content">
         {{ collection.content }}
+        {{ collection.content_two }}
+        {{ collection.content_image.url }}
         <div>
-          <img :src="collection.content_image.url" width="300" height="300" />
+          <img :src="collection.content_image.url" />
         </div>
       </div>
     </div>
+  </div>
+  <div v-else>
+    <div class="apollo-loading">
+      <h2>Im still loading..</h2>
+    </div>
+  </div> -->
+  <div>
+    {{ collection }}
   </div>
 </template>
 
@@ -34,6 +39,9 @@ export default {
         return { id: parseInt(this.$route.params.id) }
       }
     },
+    props: {
+      collection: Array
+    }
   }
 }
 </script>

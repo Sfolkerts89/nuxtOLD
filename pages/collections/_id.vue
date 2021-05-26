abb1f69ee067aefc40beb5bf32110fc803c02b96
Sven <sfolkerts89@gmail.com>
2021-05-26 23:16:04 +0200

    Fixed some previous issue: _id page, apollo loading states -- V1.3

--- pages/collections/_id.vue
+++ pages/collections/_id.vue
@@ -1,20 +1,28 @@
 <template>
-  <div v-if="$apollo.loading">
-    <div class="apollo-loading">
-      <h2>Im still loading..</h2>
+  <div>
+
+    <div v-if="$apollo.loading">
+      <div class="apollo-loading">
+        <h2>Im still loading..</h2>
+      </div>
     </div>
-  </div>
-  <div v-else>
-    <div v-if="collection" v-bind="collection" class="collection-container">
-      <h1>{{ collection.title }}</h1>
-      <div class="content">
-        {{ collection.content }}
-        <div>
-          <img :src="collection.content_image.url" width="300" height="300" />
+
+    <div v-else>
+      <div v-if="collection" v-bind="collection" class="collection-container">
+        <h1>{{ collection.title }}</h1>
+        <div class="content">
+          <p>{{ collection.content }}</p>
+          <p>{{ collection.content_two }}</p>
+          <div>
+            <img :src="collection.content_image.url" width="300" height="300" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
+ 
+  <SeoMetaData v-bind="(collection)" :SEOdata="collection" />
+
+ </div>
 </template>
 
 <script>
